1fe2b38d584ee6a9d4b40704a41faac4ce416711
Anmol Singh <singhanmol98@gmail.com>
2019-03-28 13:37:30 +0530

    abc

---
+++ server.js
@@ -0,0 +1,48 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+//app.use(express.static(__dirname+'/Public'));
+app.use((req,res,next)=> {
+	var now=new Date().toString();
+	var log=`${now}:${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log',log+ '\n');
+	next();
+});
+
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+	return text.toUpperCase();
+});
+
+app.get('/',(req,res)=>{
+	res.render('home.hbs',{
+		pageTitle:'Home Page',
+		WelcomeMessage:'Welcome to my Message',
+		currentYear: new Date().getFullYear()
+	});
+});
+app.get('/about',(req,res)=>{
+	res.render('about.hbs',{
+		pageTitle:'About Page',
+		currentYear: new Date().getFullYear()
+	});
+});
+
+app.get('/bad',(req,res)=>{
+	res.send({
+		errorMessage:'Unable to handle request'
+	});
+});
+
+app.listen(1104,()=>{
+	console.log('Server is up on port 1104');
+	});
